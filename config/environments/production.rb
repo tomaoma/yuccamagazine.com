9612f1aa300d64dc349eeaee9075ab0d962fd8e4
Ihor.R <defsan@gmail.com>
2014-09-14 17:35:40 -0700

    Changing production email domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yuccamagazine.heroku.com',
+    :domain         => 'yuccamagazine.com',
     :authentication => :plain
   }
 end
