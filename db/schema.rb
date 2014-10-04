896533595933b0f6552f2b553a695877e121a6ba
Ihor.R <defsan@gmail.com>
2014-10-04 01:28:23 -0700

    Fixing show send_all

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141004041716) do
+ActiveRecord::Schema.define(version: 20141004081145) do
 
   create_table "messages", force: true do |t|
-    t.string   "body"
+    t.text     "body",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "subject"
