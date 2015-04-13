585ad8bf5db24efe6530c667a1284e004b2a6b1b
Ihor.R <defsan@gmail.com>
2015-04-12 20:33:56 -0700

    Fixing message destroy.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ActiveRecord::Base
   has_many :subscription_messages
-  has_and_belongs_to_many :subscriptions, through: :subscription_messages
+  has_many :subscriptions, through: :subscription_messages
 
   def short_body
     ActionView::Base.full_sanitizer.sanitize(body)[0,50]
