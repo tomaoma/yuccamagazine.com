dce956efced93a0b5e44bc52f96a98af06105fa9
Ihor R <defsan@gmail.com>
2015-04-27 16:32:16 -0700

    Changing github->gitlab

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'yuccamagazine.com'
-set :repo_url, 'git@github.com:defsan/yuccamagazine.git'
+set :repo_url, 'git@gitlab.com:defsan/yuccamagazine.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
